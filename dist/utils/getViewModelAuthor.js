c80b8ae5986a99348736141675e9724133d99261
AnatoliiBass <bass_1304@ukr.net>
2024-05-10 12:51:41 +0200

    litt changes

--- dist/utils/getViewModelAuthor.js
+++ dist/utils/getViewModelAuthor.js
@@ -2,8 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getViewModelAuthor = void 0;
 function getViewModelAuthor(courses, author) {
-    console.log("getViewModel courses: ", courses);
-    console.log("getViewModel author: ", author);
     return { id: author.id, first_name: author.first_name, last_name: author.last_name, courses: courses.map(c => ({ id: c.id, name: c.name })) };
 }
 exports.getViewModelAuthor = getViewModelAuthor;
--- dist/utils/getViewModelCourse.js
+++ dist/utils/getViewModelCourse.js
@@ -2,8 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getViewModelCourse = void 0;
 function getViewModelCourse(course, author) {
-    console.log("getViewModel course: ", course);
-    console.log("getViewModel author: ", author);
     return { id: course.id, name: course.name, author: { first_name: author.first_name, last_name: author.last_name } };
 }
 exports.getViewModelCourse = getViewModelCourse;

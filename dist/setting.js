5b949619250182bb43551919105d28c21347a78d
AnatoliiBas <anatolii@advanz.no>
2024-05-30 11:28:34 +0200

    env var

--- dist/setting.js
+++ dist/setting.js
@@ -5,6 +5,6 @@ exports.setting = {
     MONGO_URI: process.env.MONGO_URI || "mongodb+srv://anatolii:oy9CmAVu1orhHfkw@cluster0.rmiojst.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     JWT_SECRET: process.env.JWT_SECRET || "secret1234",
     RESEND_API_KEY: process.env.RESEND_API_KEY || "re_HjabNQNy_4LTcXVrXcjUkMbGnU4j8j59G",
-    PROJECT_URL: process.env.PROJECT_URL || "http://localhost:3000",
+    PROJECT_URL: process.env.PROJECT_URL || "https://backendapp-rouge.vercel.app" || "http://localhost:3000",
 };
 //# sourceMappingURL=setting.js.map
